0a2838ed9de29de18e7b32820f1307fb09c6d285
anishkunwar21 <144764653+anishkunwar21@users.noreply.github.com>
2024-12-11 20:01:14 +0545

    fix-error-7

--- components/ManufacturingProcessContent.tsx
+++ components/ManufacturingProcessContent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 'use client';
 import { memo, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
--- components/sections/ManufacturingProcess.tsx
+++ components/sections/ManufacturingProcess.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 "use client"
 import React from 'react';
 import dynamic from 'next/dynamic'
