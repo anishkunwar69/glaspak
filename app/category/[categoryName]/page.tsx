165a528f9ff2a2c9a64ceb7b70730c765d6b0a55
anishkunwar69 <anishkunwar808@gmail.com>
2025-01-10 18:21:02 +0545

    fixxxxx-2

--- app/category/[categoryName]/page.tsx
+++ app/category/[categoryName]/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ProductsList from './products-list'
 
-export default async function CategoryName({ params }: { params: { categoryName: string } }) {
+interface CategoryPageProps {
+  params: {
+    categoryName: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default async function CategoryPage({ params }: CategoryPageProps) {
   return (
     <main className="min-h-screen bg-gradient-to-b from-[#F5F0EA] to-[#EDE5DB]">
       <ProductsList categoryName={params.categoryName} />
