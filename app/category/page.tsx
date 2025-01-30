e0ec93847e0acb95a7eabba61fe743fa1a2a2d4a
anishkunwar69 <anishkunwar808@gmail.com>
2025-01-30 22:25:42 +0545

    final-ever

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,13 +1,17 @@
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/sections/Footer"
 import CategoryContent from "./category-content"
+import EndContent from "@/components/EndContent"
 
 export default function(){
     return(
         <>
         <Navbar/>
         <div className="pt-[87px]">
-        <CategoryContent/>
+            <CategoryContent/>
+        </div>
+        <div className="pb-[65px] bg-[#EDE5DB]">
+            <EndContent showSeparator={true}/>
         </div>
         <Footer/>
         </>
